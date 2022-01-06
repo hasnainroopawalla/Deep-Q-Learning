5454b13e1ecf59eec33e630829444bc9f8cdecc1
hasnainroopawalla <hasnain.roopawalla@gmail.com>
2022-01-06 18:50:49 +0100

    WIP

--- dqn/__main__.py
+++ dqn/__main__.py
@@ -1,19 +1,50 @@
 import argparse
+from dqn.config import env_map
+
+
+def run(args: argparse.Namespace):
+    env = env_map[args.env]
+    print(env)
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--env",
+        type=str,
+        help="The Atari game evironment.",
+        choices=["cartpole", "pong"],
+        required=True,
+    )
+
+    parser.add_argument(
+        "--mode",
+        type=str,
+        help="Train or Evaluate (render) the RL agent.",
+        choices=["train", "evaluate"],
+        required=True,
+    )
+
+    # parser.add_argument(
+    #     "--evaluate_freq",
+    #     type=int,
+    #     default=25,
+    #     help="How often to run evaluation.",
+    #     nargs="?",
+    # )
+
+    # parser.add_argument(
+    #     "--evaluation_episodes",
+    #     type=int,
+    #     default=5,
+    #     help="Number of evaluation episodes.",
+    #     nargs="?",
+    # )
+
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--env", choices=["CartPole-v0", "CartPole-v1"])
-    parser.add_argument(
-        "--evaluate_freq",
-        type=int,
-        default=25,
-        help="How often to run evaluation.",
-        nargs="?",
-    )
-    parser.add_argument(
-        "--evaluation_episodes",
-        type=int,
-        default=5,
-        help="Number of evaluation episodes.",
-        nargs="?",
-    )
+    args = get_args()
+    run(args)
---
+++ dqn/replay_memory.py
@@ -0,0 +1,26 @@
+import random
+
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def __len__(self):
+        return len(self.memory)
+
+    def push(self, obs, action, next_obs, reward, done):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+
+        self.memory[self.position] = (obs, action, next_obs, reward, done)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        """
+        Samples batch_size transitions from the replay memory and returns a tuple
+            (obs, action, next_obs, reward)
+        """
+        sample = random.sample(self.memory, batch_size)
+        return tuple(zip(*sample))
